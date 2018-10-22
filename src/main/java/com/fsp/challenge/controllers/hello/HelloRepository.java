e9104857e14dabbf5a2ff91952205e14a474494a
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2018-10-22 09:36:36 -0400

    Cleanup

--- src/main/java/com/fsp/challenge/controllers/hello/HelloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fsp.challenge.controllers.hello;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloRepository extends JpaRepository<HelloObject, Integer> {
-
-}
